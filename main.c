395818594b4f84594f88e45b4ff672e876cef5a9
Gabriele Barberis Web <alertime1wp@gmail.com>
2021-03-08 07:13:44 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+//ESERCIZIO:
+//DARE IN INGRESSO STRINGHE NUMERICHE
+//E FARE LA SOMMA DI TUTTE
+
+#include <stdio.h>
+int i;
+int main(int argc, char *argv[]) {
+  for(i=0;i<argc;i++)
+    printf("Pos %d\t stringa: %s\n",i,argv[i]);
+  return 0;
+}
